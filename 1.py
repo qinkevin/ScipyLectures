a8903d483e64de10b884137f24574a7d2a7bc74e
qinkevin <1170549742@qq.com>
2016-11-08 13:57:53 +0800

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,7 @@
+"A demo module."
+def print_b(): "Prints b."
+print  b 
+def print_a(): "Prints a."
+print  a 
+c=2
+d=2
---
+++ demo.py
@@ -0,0 +1,10 @@
+"A demo module."
+def print_b(): "Prints b."
+print  'b'
+def print_a(): "Prints a."
+print  'a'
+print_b()
+if __name__ == '__main__':
+	print_a()
+c=2
+d=2
